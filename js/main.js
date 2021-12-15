6849a2005dcb7f070a224ef7b97a2f8c5dd89fee
zzinis <lizzyin@naver.com>
2021-12-15 12:02:19 +0900

    sld5 article 추가

--- js/main.js
+++ js/main.js
@@ -30,3 +30,20 @@ const swiper = new Swiper("#wrap",{
     // }
 
 });
+
+
+// (function($){
+//     var pathes = $('#port').find('path');
+//     pathes.each(function( i, path ) {
+//         var total_length = path.getTotalLength(); // 1번 부분
+ 
+//         path.style.strokeDasharray = total_length + " " + total_length; // 2번 부분
+//         path.style.strokeDashoffset = total_length; // 3번 부분
+ 
+//         // 4번 부분
+//         $(path).animate({
+//             "strokeDashoffset" : 0
+//         }, 1500);
+//     });
+//     //}
+// })(jQuery);
