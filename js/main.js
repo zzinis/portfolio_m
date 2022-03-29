85526565c8fffd9cb77ac0869388e244d6af0ec3
zzins <lizzyin@naver.com>
2022-03-29 19:29:05 +0900

    sld6 숨김처리

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const menu = ["About", "Jquery Website","JS Website", "Swiper", "Isotope", "React Website", "Contact"]
+const menu = ["About", "Jquery Website","JS Website", "Swiper", "Isotope", "Contact"]
 
 const swiper = new Swiper("#wrap",{
 
@@ -21,29 +21,7 @@ const swiper = new Swiper("#wrap",{
             return`<span class="${className}">${menu[index]}</span>`
         }
     },
-    // effect :"coverflow",
-    // coverflowEffect:{
-    //     rotate:50,
-    //     stretch: -100,
-    //     depth: 400,
-    //     slideShadow: false
-    // }
+
 
 });
 
-
-// (function($){
-//     var pathes = $('#port').find('path');
-//     pathes.each(function( i, path ) {
-//         var total_length = path.getTotalLength(); // 1번 부분
- 
-//         path.style.strokeDasharray = total_length + " " + total_length; // 2번 부분
-//         path.style.strokeDashoffset = total_length; // 3번 부분
- 
-//         // 4번 부분
-//         $(path).animate({
-//             "strokeDashoffset" : 0
-//         }, 1500);
-//     });
-//     //}
-// })(jQuery);
